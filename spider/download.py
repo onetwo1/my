bd84ddf5bf74221eef24fbbdcb8cb41b26c796d3
onetwo1 <1139409573@qq.com>
2018-01-17 11:32:06 +0800

    spider

--- spider/download.py
+++ spider/download.py
@@ -3,7 +3,13 @@ import MySQLdb
 from threading import Thread
 from queue import Queue
 from scrapy import Selector
-
+import re
+import base64
+import os
+import threading
+from mysql_client_orm import BaiXing
+from urllib import parse
+import time
 
 # 生产者 and 消费者队列模式
 
@@ -15,96 +21,169 @@ Url_Set = set()
 Res_Queue = Queue()
 # 存储data的队列
 Data_Queue = Queue()
-
+# 结束信号
+signal_ = object
 
 # 放入url的类
 class Producer(Thread):
 	def __init__(self):
-		super(Producer, self).__init__()
-	
+		super().__init__()
+
 	def run(self):
 		"""任务函数 -将url放入队列- """
 		while True:
-			# 如果队列没有满
-			if My_Queue.not_full():
-				# 从集合中取出一个url放入队列中
+			print('producer')
+			# 从集合中取出一个url放入队列中
+			if len(Url_Set):
 				My_Queue.put(Url_Set.pop())
+			else:
+				print('放入url完成')
+				break
 
 # 消费url的类
 class Consumer(Thread):
 	def __init__(self):
-		super(Consumer, self).__init__()
-	
+		super().__init__()
 	# 下载数据 - 接收参数url
 	def download_task(self, url):
-		res = requests.get(url=url)
+		netloc = parse.urlparse(url).netloc
+		headers = {
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+			'Accept-Encoding': 'gzip, deflate',
+			'Accept-Language': 'zh-CN,zh;q=0.9',
+			'Cache-Control': 'max-age=0',
+			'Connection': 'keep-alive',
+			'Host': netloc,
+			'Upgrade-Insecure-Requests': '1',
+			'User - Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+							'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+		}
+		res = requests.get(url=url, headers=headers)
+		time.sleep(2)
 		return res
 	
 	def run(self):
 		"""任务函数 -消耗url- """
 		while True:
-			# 如果队列不为空
-			if My_Queue.not_empty():
+			if not My_Queue.empty():
+				print('consumer')
 				# 从队列中取出一个url执行下载任务
 				url = My_Queue.get()
+				# 大致大小
+				print('当前需要下载的url还有{}个'.format(My_Queue.qsize()))
 				res = self.download_task(url)
 				# 返回的响应放入一个队列中
 				Res_Queue.put(res)
-				
+			else:
+				Res_Queue.put(signal_)
+				break
 # 解析res的类
 class Parser(Thread):
 	def __init__(self):
-		super(Parser, self).__init__()
-	
+		super().__init__()
+		self.pattern = re.compile(r'.*?\((.*?)\).*?')
+		self.media_path = os.path.join(os.path.dirname(os.getcwd()), 'media')
+
+	# 创建媒体文件夹 - 根据base_url命名 - base_url进行b64加密
+	def mkdir_media(self, base_url):
+		# 对base_url进行b64加密 - 只是作为文件名来区分
+		file_name = base64.b64encode(base_url.encode('utf-8'))
+		file_name = str(file_name, 'utf-8')
+		file_path = os.path.join(self.media_path,file_name)
+		# 在media文件下创建同名文件夹
+		if not os.path.exists(file_path):
+			os.mkdir(file_path)
+		return file_path
+
+	# 根据url判断是图片或者MP4视频文件生成空文件
+	def mk_stream_file_name(self, url, file_path):
+		pattern = re.compile(r'.*(\..*)$')
+		file_type = pattern.match(url).group(1)
+		media_file_name = str(base64.b64encode(url.encode('utf-8')), 'utf-8')[:8]
+		full_path = os.path.join(file_path, media_file_name, file_type)
+		with open(full_path, 'wb') as f:
+			return full_path
+
+	# 下载流媒体
+	def download_media(self, url, file_path, start, end):
+		# 开启下载
+		headers = {'Range': 'bytes=%d-%d' % (start, end)}
+		r = requests.get(url, headers=headers, stream=True)
+
+		# 写入文件对应位置
+		with open(file_path, "rb") as fp:
+			fp.seek(start)
+			fp.write(r.content)
+
+
 	# 解析数据 - 接受参数res
 	def parse_task(self, res):
-		selector_res = Selector(res)
+		# 获得存储media文件的路径
 		url = res.request.url
-		title = selector_res.xpath('//div[@class="viewad-title"]/h1/text()').extract_first()
-		price = selector_res.xpath('//div[@class="viewad-actions"]/span[@class="price"]/text()').extract_first().strip()
-		publish_date = selector_res.xpath('//div[@class="viewad-actions"]/span[2]/text()').extract_first()
-		h_publish_date = selector_res.xpath('//div[@class="viewad-actions"]/span[2]/@data-original-title').extract_first()
-		varieties = selector_res.xpath('//div[@class="top-meta clearfix"]/li[1]/a/text()').extract_first()
-		gender = selector_res.xpath('//div[@class="top-meta clearfix"]/li[2]/span[2]/text()').extract_first()
-		age = selector_res.xpath('//div[@class="top-meta clearfix"]/li[3]/span[2]/text()').extract_first()
-		publisher_id = selector_res.xpath('//span[@class="meta-posterType"]/text()').extract_first()
-		supply_and_demand = selector_res.xpath('//span[@class="meta-供求"]/text()').extract_first()
-		address = selector_res.xpath('//span[@class="meta-address"]/a/text()').extract_first()
+		# file_path = self.mkdir_media(url)
+		selector_res = Selector(res)
 		phone_1 = selector_res.xpath('//a[@class="contact-no"]/text()').extract_first()
 		phone_2 = selector_res.xpath('//a[@class="show-contact"]/@data-contact').extract_first()
-		phone = phone_1.replace('*', '') + phone_2
-		weChat = ''
-		ancestry = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[1]/label[2]/text()').extract_first()
-		vaccine = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[3]/label[2]/text()').extract_first()
-		insect = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[4]/label[2]/text()').extract_first()
-		sale_number = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[5]/label[2]/text()').extract_first()
-		trading_place = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[6]/label[2]/text()').extract_first()
-		desc = selector_res.xpath('//div[@class="viewad-text-hide"]/text()').extract()
+		if phone_1 and phone_2:
+			phone = phone_1.replace('*', '') + phone_2
+		else:
+			phone = ''
 		picture_flag = selector_res.xpath('//div[@class="featured-height"]')
 		if picture_flag:
 			photo_item = picture_flag.xpath('div/a/@style').extract()
+			photo_item = ','.join([self.pattern.match(photo_url).group(1) for photo_url in photo_item])
+			# 下载保存图片的操作
+			picture_url = photo_item
 		else:
 			picture_url = ''
 		video_flag = selector_res.xpath('//source[@type="video/mp4"]')
 		if video_flag:
 			video_url = video_flag[0].xpath('@src').extract_first(default='')
-		pass
+		else:
+			video_url = ''
+
+		data = dict(
+			url = url,
+			title = selector_res.xpath('//div[@class="viewad-title"]/h1/text()').extract_first(),
+			price = selector_res.xpath('//div[@class="viewad-actions"]/span[@class="price"]/text()').extract_first(),
+			publish_date = selector_res.xpath('//div[@class="viewad-actions"]/span[2]/text()').extract_first(),
+			varieties = selector_res.xpath('//div[@class="top-meta clearfix"]/li[1]/a/text()').extract_first(),
+			gender = selector_res.xpath('//div[@class="top-meta clearfix"]/li[2]/span[2]/text()').extract_first(),
+			age = selector_res.xpath('//div[@class="top-meta clearfix"]/li[3]/span[2]/text()').extract_first(),
+			publisher_id = selector_res.xpath('//span[@class="meta-posterType"]/text()').extract_first(),
+			supply_and_demand = selector_res.xpath('//span[@class="meta-供求"]/text()').extract_first(),
+			address = selector_res.xpath('//span[@class="meta-address"]/a/text()').extract_first(),
+			weChat = selector_res.xpath('//div[@class="weixin-contact-promo"]/'
+			                           'div[@class="detail"]/text()').extract_first(default=''),
+			ancestry = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[1]/label[2]/text()').extract_first(),
+			vaccine = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[3]/label[2]/text()').extract_first(),
+			insect = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[4]/label[2]/text()').extract_first(),
+			sale_number = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[5]/label[2]/text()').extract_first(),
+			trading_place = selector_res.xpath('//div[@class="viewad-detail"]/div[@class="viewad-meta2"]/div[6]/label[2]/text()').extract_first(),
+			desc = ','.join(selector_res.xpath('//div[@class="viewad-text-hide"]/text()').extract()),
+			picture_url = picture_url,
+			video_url = video_url,
+		)
+		return data
 
 	def run(self):
 		"""任务函数 -消耗res- """
 		while True:
-			if Res_Queue.not_empty():
+			if not Res_Queue.empty():
+				print('parse')
 				# 从队列中取出一个res进行解析
 				res = Res_Queue.get()
+				if res is signal_:
+					Res_Queue.put(signal_)
+					break
+				print('当前需要解析的res还有{}个'.format(Res_Queue.qsize()))
 				data = self.parse_task(res)
 				# 放入data队列
 				Data_Queue.put(data)
 
 
 
-
 if __name__ == '__main__':
-	
 	# 从文本中取出url - 放入set集合中
 	with open('id.txt', 'r', encoding='utf-8') as f:
 		for line in f.readlines():
@@ -112,10 +191,38 @@ if __name__ == '__main__':
 				continue
 			else:
 				Url_Set.add(line)
-	sc = Consumer()
-	sp = Parser()
-	res = sc.download_task('http://ezhou.baixing.com/chongwujiaoyi/a1253977347.html')
-	sp.parse_task(res)
 
 
-	
+	threads = []
+	# 放入url
+	put_url = Producer()
+	threads.append(put_url)
+	# 请求url
+	for i in range(3):
+		get_url = Consumer()
+		parse_url = Parser()
+		threads.append(get_url)
+		threads.append(parse_url)
+
+	for t in threads:
+		t.setDaemon(True)
+		t.start()
+	t.join()
+	print('线程执行完成')
+	Data_Queue.put(signal_)
+	# 入库
+	client = BaiXing()
+	session = client.conn()
+	while True:
+		# 创建一条数据
+		data = Data_Queue.get()
+		if data is signal_:
+			break
+		new_data = BaiXing(**data)
+		# 添加到seesion
+		session.add(new_data)
+		# 提交
+		session.commit()
+	session.close()
+
+
--- spider/mysql_client_orm.py
+++ spider/mysql_client_orm.py
@@ -9,56 +9,55 @@ Base = declarative_base()
 
 # 数据表类
 class BaiXing(Base):
-    def __init__(self):
-        __tablename__ = 'BaiXing'
-        # 表结构
-        '''
-        id                   - 主键
-        url                  - 请求的url
-        title                - 标题
-        price                - 价格
-        publish_date         - 发布时间
-        h_publish_date       - 历史发布时间
-        varieties            - 品种
-        gender               - 性别
-        age                  - 年龄
-        publisher_id         - 发布人身份
-        supply_and_demand    - 供求
-        address              - 地址
-        phone                - 手机号
-        weChat               - 微信号
-        ancestry             - 血统
-        vaccine              - 疫苗情况
-        insect               - 驱虫情况
-        sale_number          - 待售只数
-        trading_place        - 交易地点
-        desc                 - 描述
-        picture_url          - 照片链接 是一个列表 存放多个照片的url
-        video_url            - 视频链接 应该只要一个即可
-        '''
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        url = Column(String(200))
-        title = Column(String(200))
-        price = Column(String(200))
-        publish_date = Column(String(200))
-        h_publish_date = Column(String(200))
-        varieties = Column(String(200))
-        gender = Column(String(200))
-        age = Column(String(200))
-        publisher_id = Column(String(200))
-        supply_and_demand = Column(String(200))
-        address = Column(String(200))
-        phone = Column(String(200))
-        weChat = Column(String(200))
-        ancestry = Column(String(200))
-        vaccine = Column(String(200))
-        insect = Column(String(200))
-        sale_number = Column(String(200))
-        trading_place = Column(String(200))
-        desc = Column(Text)
-        picture_url = Column(String(200))
-        video_url = Column(String(200))
+    __tablename__ = 'bai_xing'
+    # 表结构
+    '''
+    id                   - 主键
+    url                  - 请求的url
+    title                - 标题
+    price                - 价格
+    publish_date         - 发布时间
+    varieties            - 品种
+    gender               - 性别
+    age                  - 年龄
+    publisher_id         - 发布人身份
+    supply_and_demand    - 供求
+    address              - 地址
+    phone                - 手机号
+    weChat               - 微信号
+    ancestry             - 血统
+    vaccine              - 疫苗情况
+    insect               - 驱虫情况
+    sale_number          - 待售只数
+    trading_place        - 交易地点
+    desc                 - 描述
+    picture_url          - 照片链接 是一个列表 存放多个照片的url
+    video_url            - 视频链接 应该只要一个即可
+    '''
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(200))
+    title = Column(String(200))
+    price = Column(String(200))
+    publish_date = Column(String(200))
+    varieties = Column(String(200))
+    gender = Column(String(200))
+    age = Column(String(200))
+    publisher_id = Column(String(200))
+    supply_and_demand = Column(String(200))
+    address = Column(String(200))
+    phone = Column(String(200))
+    weChat = Column(String(200))
+    ancestry = Column(String(200))
+    vaccine = Column(String(200))
+    insect = Column(String(200))
+    sale_number = Column(String(200))
+    trading_place = Column(String(200))
+    desc = Column(Text)
+    picture_url = Column(String(200))
+    video_url = Column(String(200))
 
+    def conn(self):
+        # 创建一个链接实例
         # 初始化数据库链接
         engine = create_engine('mysql+mysqldb://root:1139409573@localhost:3306/baixing?charset=utf8',
                                convert_unicode=True)
@@ -66,9 +65,6 @@ class BaiXing(Base):
         Base.metadata.create_all(engine)
 
         # 创建数据库链接
-        self.db_session = sessionmaker(bind=engine)
-
-    def conn(self):
-        # 创建一个链接实例
-        session = self.db_session()
+        db_session = sessionmaker(bind=engine)
+        session = db_session()
         return session
